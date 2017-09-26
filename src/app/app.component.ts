6a38ce15e45baffde5d1bfa3c5f40f512b299e3c
danielcb29 <danielc.web@gmail.com>
2017-09-25 20:31:27 -0500

    D3 import

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import * as d3 from 'd3';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    d3.select('.main')
+  }
+}
