09df7e541de4ca40544ca38389041fef5dd55a7d
danielcb29 <danielc.web@gmail.com>
2017-09-30 22:13:23 -0500

    Add svg circle to master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,59 @@ import { OnInit } from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
-  constructor() {
-  }
+    svgData = [
+        {
+            height: 100,
+            width: 100,
+        }
+    ];
 
-  ngOnInit() {
-    d3.select('.main')
-  }
+    cirlceData = [{
+        cx: 50,
+        cy: 50,
+        r: 40,
+        stroke: 'black',
+        strokeWidth: 3,
+        fill: 'green'
+    }];
+
+    svg = null;
+
+    constructor() {
+    }
+
+    initSVG() {
+        this.svg = d3.select('.main')
+            .selectAll('svg')
+            .data(this.svgData)
+            .enter()
+            .append('svg')
+            .attr('height', d => d.height)
+            .attr('width', d => d.width);
+    }
+
+    addCircle() {
+        this.svg
+            .selectAll('svg')
+            .data(this.cirlceData)
+            .enter()
+            .append('circle')
+            .attr('cx', d => d.cx)
+            .attr('cy', d => d.cy)
+            .attr('r', d => d.r)
+            .attr('stroke', d => d.stroke)
+            .attr('stroke-width', d => d.strokeWidth)
+            .attr('fill', d => d.fill);
+    }
+
+    ngOnInit() {
+        this.initSVG();
+        this.addCircle();
+    }
 }
